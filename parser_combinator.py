5efc999d814f2cb61f33a69a77af43da87f34826
Sarp <54453420+Ricider@users.noreply.github.com>
2020-10-13 19:07:29 +0300

    Update parser_combinator.py

--- parser_combinator.py
+++ parser_combinator.py
@@ -4,6 +4,8 @@ parse_char = lambda s,char: (s[0],s[1:]) if len(s)>0 and s[0]==char else (None,s
 
 def parse_int(s):
     out=""
+    sign,s=parse_char(s,"-")
+    if sign: out="-"
     while True:
         curr,s = parse_decimal(s)
         if curr==None: break
@@ -15,10 +17,10 @@ def parse_paranthesis(s):
     
     begp,s=parse_char(s,"(")
     if begp==None: return parse_int(unmodified_s)
-    first,s=parse_addition(s)
+    first,s=parse_expr(s)
     endp,s=parse_char(s,")")   
     
-    if None in [endp,begp,first]: return parse_addition(unmodified_s)
+    if None in [endp,begp,first]: return parse_expr(unmodified_s)
     else: return (first,s)
     
 def parser_template(upper_func_name,func_op,func_symbol,s):
@@ -38,13 +40,13 @@ parse_multiplication=apply_template(parse_paranthesis,lambda x,y:x*y,"*")
 parse_division=apply_template(parse_multiplication,lambda x,y:x//y,"/")
 parse_subtraction=apply_template(parse_division,lambda x,y:x-y,"-")
 parse_addition=apply_template(parse_subtraction,lambda x,y:x+y,"+")
-    
+
 def parse_expr(s):
-    s=s.replace(" ","")
-    while True:
-        curr,s = parse_addition(s)
-        if curr==None or len(s) == 0: break
-        s=str(curr)+s
-    return (curr,s)    
+    first,s=parse_addition(s)
+    pls_sign,s=parse_char(s,"+")
+    second,s=parse_addition(s)
+    
+    if None in [pls_sign,second]: return (first,s)
+    else: return (int(first)+int(second),s)    
 
 while True: print(parse_expr(input("enter input expression: ")))
