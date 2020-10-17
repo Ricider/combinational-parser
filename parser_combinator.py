01b1650b3abe4971c2366a2240dfb50afa77416c
Sarp <54453420+Ricider@users.noreply.github.com>
2020-10-17 22:01:26 +0300

    Update parser_combinator.py

--- parser_combinator.py
+++ parser_combinator.py
@@ -1,43 +1,38 @@
-parse_decimal = lambda s: (s[0],s[1:]) if len(s)>0 and s[0].isnumeric() else (None,s) 
+parse_decimal = lambda s: (s[-1],s[:-1]) if len(s)>0 and s[-1].isnumeric() else (None,s) 
 
-parse_char = lambda s,char: (s[0],s[1:]) if len(s)>0 and s[0]==char else (None,s) 
+parse_char = lambda s,char: (s[-1],s[:-1]) if len(s)>0 and s[-1]==char else (None,s) 
 
 def parse_int(s):
-    sign,s=parse_char(s,"-")
-    out = "-" if sign else ""
+    out = ""
+    sign=None
     while True:
         curr,s = parse_decimal(s)
         if curr==None: break
-        out+=curr
-    return (out if out else None,s)
+        out=curr+out
+    if len(s) >= 2 and not (s[-2].isnumeric() or s[-2]==")"): sign,s=parse_char(s,"-")    
+    return (("-"+out if sign else out) if out else None,s)
     
 def parse_paranthesis(s):
-    unmodified_s=s
-    
-    begp,s=parse_char(s,"(")
-    if begp==None: return parse_int(unmodified_s)
+    begp,s=parse_char(s,")")
+    if begp==None: return parse_int(s)
     first,s=parse_addition(s)
-    endp,s=parse_char(s,")")   
-    
-    if None in [endp,begp,first]: return parse_addition(unmodified_s)
-    else: return (first,s)
+    endp,s=parse_char(s,"(")   
+    return (first,s)
     
 def parser_template(upper_func_name,func_op,func_symbol,s):
-    unmodified_s=s
-    
     first,s=upper_func_name(s)
     pls_sign,s=parse_char(s,func_symbol)
-    if pls_sign==None: return upper_func_name(unmodified_s)
+    if pls_sign==None: return (first,s)
     second,s=parser_template(upper_func_name,func_op,func_symbol,s)
     
-    if None in [first,pls_sign,second]:return upper_func_name(unmodified_s)
-    else: return (func_op(int(first),int(second)),s)
+    if not second: return (first,s)
+    else: return (func_op(int(second),int(first)),s)
     
 apply_template = lambda upper_func_name,func_op,func_symbol: lambda s: parser_template(upper_func_name,func_op,func_symbol,s)
     
 parse_multiplication=apply_template(parse_paranthesis,lambda x,y:x*y,"*")
 parse_division=apply_template(parse_multiplication,lambda x,y:x//y,"/")
 parse_subtraction=apply_template(parse_division,lambda x,y:x-y,"-")
-parse_addition=apply_template(parse_subtraction,lambda x,y:x+y,"+") 
+parse_addition=apply_template(parse_subtraction,lambda x,y:x+y,"+")
 
 while True: print(parse_addition(input("enter input expression: ")))
